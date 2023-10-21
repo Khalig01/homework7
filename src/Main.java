a85f84ba1a8cbc29aac2e09ea4f1a2a3fa904515
Khal01 <aliyev_56@list.ru>
2023-10-21 10:18:03 +0300

    7.1

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+
+    System.out.println("Задача1");
+int totally= 0;
+int everyM= 15000;
+int i =0  ;
+while(totally<= 2_459_000){
+totally= totally + everyM;
+   totally= totally ++;
+    System.out.println("Месяц" + i + "Итого" + totally);
+
+}
+
+        System.out.println("Задача2");
+int start= 0;
+while (start<=10){
+    System.out.print ( start );
+    start= start+ 1;
+    }
+        System.out.println();
+        for (int b = 10; b >=1; b--){
+
+            System.out.print (b );
+            System.out.println( );
+        }
+        System.out.println("Задача3");
+
+
+
+
+
+
+
+
+
+
+}
+}
